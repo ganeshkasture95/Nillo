7eca6b5ece29fee8ad743e3564ef636df8fc2cd6
Ganesh Kasture <ganeshkasture9595@gmail.com>
2024-09-24 17:16:15 +0530

    added bottom sheet modal to auth

--- components/AuthModel.tsx
+++ components/AuthModel.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react"
 import { Text, View } from "react-native"
+import { ModalType } from "types/enums"
 
-const AuthModel = () => {
+
+interface AuthModelProps{
+    authType: ModalType | null
+}
+
+const AuthModel = ({authType}:AuthModelProps) => {
+
+  useEffect(()=>{
+    console.log('AuthModel', authType)
+  })
+
   return (
     <View>
       <Text>AuthModel</Text>
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,50 +1,101 @@
+import { useActionSheet } from "@expo/react-native-action-sheet";
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider
+} from '@gorhom/bottom-sheet';
+import AuthModel from "components/AuthModel";
 import { Colors } from "constants/Colors";
 import * as WebBrowser from "expo-web-browser";
-import React from "react";
+import React, { useMemo, useRef, useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { ModalType } from "types/enums";
 
-
 export default function Page() {
 
   const { top } = useSafeAreaInsets()
-  const showModal = async (type: ModalType) => {
+  const { showActionSheetWithOptions } = useActionSheet()
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const snapPoints = useMemo(() => ['33%','90%'], []);
 
+  const [authType, setAuthType] = useState<ModalType | null>(null)
 
-  }
-  const openLink=()=>{
+  const openLink = () => {
     WebBrowser.openBrowserAsync("https://ganeshkasture.vercel.app")
   }
-  return (
-    <View style={[styles.container,
-    {
-      paddingTop: top + 30
-    }
-    ]}>
+  const showModal = async (type: ModalType) => {
+    setAuthType(type),
+      bottomSheetModalRef.current?.present();
+  }
 
-      <Image source={require("../../assets/images/login/trello.png")} style={styles.image} />
-      <Text style={styles.introText}>Move teamwork forword - even on the go</Text>
-      <View style={styles.bottomContainer}>
-        <TouchableOpacity style={[styles.btn, { backgroundColor: "white" }]} onPress={() => { showModal(ModalType.Login) }}>
-          <Text style={(styles.btnText, { color: Colors.primary })}>
-            Log in
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.btn, { backgroundColor: "transparent" }]} onPress={() => { showModal(ModalType.SignUp) }}>
-          <Text style={(styles.btnText, { color: Colors.fontLight })}>
-            Sign up
-          </Text>
-        </TouchableOpacity>
+
+
+  const openSctionSheet = async () => {
+    const option = ['View support docs', 'Contct us', 'Cancel'];
+    const cancelButtonIndex = 2;
+
+    showActionSheetWithOptions(
+      {
+        options: option,
+        cancelButtonIndex,
+        title: "Can't Log in please Signup?"
+      },
+      (selectedIndex: any) => {
+        if (selectedIndex === 0) {
+          WebBrowser.openBrowserAsync("https://ganeshkasture.vercel.app")
+        }
+        if (selectedIndex === 1) {
+          WebBrowser.openBrowserAsync("https://ganeshkasture.vercel.app")
+        }
+        if (selectedIndex === 2) {
+          console.log("Cancel")
+        }
+      }
+    );
+  }
+
+  return (
+    <BottomSheetModalProvider>
+      <View style={[styles.container, { paddingTop: top + 30 }]}>
+
+        <Image source={require("../../assets/images/login/trello.png")} style={styles.image} />
+
+        <Text style={styles.introText}>Move teamwork forward - even on the go</Text>
+        <View style={styles.bottomContainer}>
+          <TouchableOpacity style={[styles.btn, { backgroundColor: "white" }]} onPress={() => { showModal(ModalType.Login) }}>
+            <Text style={(styles.btnText, { color: Colors.primary })}>
+              Log in
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.btn, { backgroundColor: "transparent" }]} onPress={() => { showModal(ModalType.SignUp) }}>
+            <Text style={(styles.btnText, { color: Colors.fontLight })}>
+              Sign up
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <Text style={styles.description} >
+          By signing up, you agree to the <Text style={styles.link} onPress={openLink}>User Notice</Text> and <Text style={[styles.link]} onPress={openLink}>Privacy Policy</Text>
+        </Text>
+
+        <Text style={styles.link} onPress={openSctionSheet}>Can't Log in please Signup?</Text>
       </View>
 
-      <Text style={styles.description} >
-        By signing up, you agree to the <Text style={styles.link} onPress={openLink}>User Notice</Text> and <Text style={[styles.link]} onPress={openLink}>Privacy Policy</Text>
-      </Text>
 
-      <Text style={styles.link} onPress={openLink}>Can't Log in please Signup</Text>
-    </View>
+{/* this is the bottom sheet in the app using gorhomes bottom sheet  */}
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={0}
+        snapPoints={snapPoints}
+      >
+        <View>
+         <AuthModel  authType= {authType}/>
+        </View>
+
+      </BottomSheetModal>
+
+    </BottomSheetModalProvider>
   );
 }
 
